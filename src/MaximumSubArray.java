72dcfa0a069d1c796e7d5f584663198e369fe2c3
klaus19 <tejaskhartude@gmail.com>
2022-10-06 09:51:10 +0530

    Added maximumsUBaRRAY PROBLEM

--- src/MaximumSubArray.java
+++ src/MaximumSubArray.java
@@ -13,6 +13,19 @@ public class MaximumSubArray {
         return max_sum;
     }
 
+    //second way
+    public int getMaxArray(int []arr){
+        int max_sum=arr[0] ,n=arr.length;
+
+        for (int i=1;i<n;i++){
+
+            if (arr[i-1]>0) arr[i]=arr[i-1]+arr[i];
+            if (max_sum<arr[i]) max_sum=arr[i];
+        }
+        return max_sum;
+    }
+    
+
     public static void main(String[] args) {
         MaximumSubArray mx = new MaximumSubArray();
         int []arr = new int[]{1,-3,4,-1};
