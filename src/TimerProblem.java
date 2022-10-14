c6c4723ce35da2595ef7cad1e9ef19ba9d2d2486
klaus19 <tejaskhartude@gmail.com>
2022-10-14 21:37:36 +0530

    added a timer problem

---
+++ src/TimerProblem.java
@@ -0,0 +1,8 @@
+public class TimerProblem {
+
+    int getTimer(int n){
+        int hours = n/60;
+        int minutes = n%60;
+        return hours/10 + hours%10 + minutes/10 + minutes%10;
+    }
+}
