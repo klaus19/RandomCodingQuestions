d2af55f097e2575cce601648e0e7423f13b61e90
klaus19 <tejaskhartude@gmail.com>
2022-10-20 18:55:37 +0530

    Added Jump I problem

---
+++ src/Jump1.java
@@ -0,0 +1,17 @@
+public class Jump1 {
+
+    public boolean canJump(int[]nums){
+        int reachable = 0;
+        for (int i=0; i<nums.length; ++i) {
+            if (i > reachable) return false;
+            reachable = Math.max(reachable, i + nums[i]);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Jump1 jp = new Jump1();
+        int[]arr = new int[]{2,3,1,1,4};
+        System.out.println(jp.canJump(arr));
+    }
+}
