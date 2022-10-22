d4902c13648e0065b59764d93d7c2aa15e09081c
klaus19 <tejaskhartude@gmail.com>
2022-10-22 18:41:30 +0530

    aDDED A NEW Problem

--- src/CountConsistentStrings.java
+++ src/CountConsistentStrings.java
@@ -1,4 +1,7 @@
+import javax.xml.stream.events.Characters;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CountConsistentStrings {
 
@@ -6,13 +9,34 @@ public class CountConsistentStrings {
     // Output :- 2;
     public int countConsistentStrings(String allowed, String[] words) {
 
-         int count=0;
-          for (int i=0;i<words.length;i++){
-              if (words[i].contains(allowed))
-                  count++;
-          }
-        return count;
+//         int count=0;
+//          for (int i=0;i<words.length;i++){
+//              if (words[i].contains(allowed))
+//                  count++;
+//          }
+//        return count;
 
+        Set<Character> set = new HashSet<>();
+        for (char c : allowed.toCharArray()) {
+            set.add(c);
+        }
+        int count = 0;
+        for (String word : words) {
+            int flag = 0;
+            for (char ch : word.toCharArray()) {
+                if (set.contains(ch))
+                    flag = 1;
+                else {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag == 1) {
+                count += 1;
+            }
+
+        }
+        return count;
     }
 
     public static void main(String[] args) {
