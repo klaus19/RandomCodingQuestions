d59368388e766cc51e602b50e48b2700db7051eb
klaus19 <tejaskhartude@gmail.com>
2022-10-27 12:51:09 +0530

    string is Rotated

---
+++ src/RotateString.java
@@ -0,0 +1,18 @@
+public class RotateString {
+
+    public boolean rotateString(String A, String B) {
+
+        if(A.length()==0 && B.length()==0){
+            return true;
+        }
+        StringBuilder d=new StringBuilder(A);
+        for(int i=0;i<A.length();i++){
+            d.append(d.charAt(0));
+            d.deleteCharAt(0);
+            if(d.toString().equals(B)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
