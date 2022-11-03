d8d654b0b614a315ad6e30cf07810ec736c37506
klaus19 <tejaskhartude@gmail.com>
2022-11-03 15:55:46 +0530

    added power to problem

---
+++ src/Maths/Power2.java
@@ -0,0 +1,21 @@
+package Maths;
+
+public class Power2 {
+
+    public boolean isPowerOfTwo(int n) {
+        if(n==0)
+            return false;
+        if(n==1)
+            return true;
+        if(n%2==0)
+            return isPowerOfTwo(n/2);
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        Power2 p2 = new Power2();
+        int n=3;
+        System.out.println(p2.isPowerOfTwo(n));
+    }
+}
