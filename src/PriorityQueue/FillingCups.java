06f6462362ea12dff3ecbe38ec787cdab11a5d31
klaus19 <tejaskhartude@gmail.com>
2022-11-04 20:48:31 +0530

    filling cups problem added

---
+++ src/PriorityQueue/FillingCups.java
@@ -0,0 +1,24 @@
+package PriorityQueue;
+
+import java.util.Arrays;
+
+public class FillingCups {
+
+    public int fillCups(int[] amount) {
+        Arrays.sort(amount);
+        int x=amount[0];
+        int y=amount[1];
+        int z=amount[2];
+        int sum=x+y+z;
+        if(x+y>z){return sum/2 +sum%2;}
+        if(x==0&&y==0){return z;}
+        else{return z;}
+    }
+
+    public static void main(String[] args) {
+        FillingCups fp = new FillingCups();
+        int[]arr = new int[]{1,4,2};
+        System.out.println(fp.fillCups(arr));
+    }
+
+}
