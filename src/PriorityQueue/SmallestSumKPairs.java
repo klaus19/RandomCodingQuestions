e41b28dc82aa8ded970050412a613e86796b37d9
klaus19 <tejaskhartude@gmail.com>
2022-11-07 12:36:25 +0530

    Added the problem of smallest k PAIRS

---
+++ src/PriorityQueue/SmallestSumKPairs.java
@@ -0,0 +1,36 @@
+package PriorityQueue;
+
+import java.util.*;
+
+public class SmallestSumKPairs {
+
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+
+        //In the minHeap array, 0th element refers to the curr element in nums1 and 1st element refers to curr element in nums2
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> (a[0] + a[1]) - (b[0] + b[1]));
+
+        // The 2nd element in the minHeap is the index of nums2, the corresponding element of nums2 is in index1 of minHeap
+        for(int i=0; i < nums1.length && i < k; i++)
+            minHeap.add(new int[]{nums1[i], nums2[0], 0});
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        for(int i=0; i < k && !minHeap.isEmpty(); i++){
+            int[] curr = minHeap.poll();
+            result.add(List.of(curr[0], curr[1]));
+            int nums2Idx = curr[2];
+            if(nums2Idx < nums2.length - 1)
+                minHeap.add(new int[]{curr[0], nums2[nums2Idx + 1], nums2Idx + 1});
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        SmallestSumKPairs sk = new SmallestSumKPairs();
+        int[]arr1 = new int[]{1,7,11};
+        int[]arr2 = new int[]{2,4,6};
+        int t=3;
+        System.out.println(sk.kSmallestPairs(arr1,arr2,t));
+    }
+
+}
