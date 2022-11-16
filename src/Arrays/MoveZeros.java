87d7771901615b86d6031d416cb5743790bb85b1
klaus19 <tejaskhartude@gmail.com>
2022-11-16 22:29:24 +0530

    Added a new problem

---
+++ src/Arrays/MoveZeros.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class MoveZeros {
+
+    public void moveZeroes(int[] nums) {
+
+        int ln = nums.length-1, count=0, j=0;
+        for (int i=0; i<=ln; i++) {
+            if(nums[i]!=0){
+                if(count==0){
+                    j++;
+                } else{
+                    nums[j]=nums[i];
+                    j++;
+                }
+            } else {
+                count++;
+            }
+        }
+        while(count>0){
+            nums[ln] = 0;
+            ln--;
+            count--;
+        }
+
+    }
+
+}
