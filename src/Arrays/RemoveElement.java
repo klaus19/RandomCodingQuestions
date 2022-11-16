e6b528a16aad291a919e5a5eaa40977c90a15462
klaus19 <tejaskhartude@gmail.com>
2022-11-16 17:32:16 +0530

    added remove element

---
+++ src/Arrays/RemoveElement.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class RemoveElement {
+
+    public int removeElement(int[] nums, int val) {
+
+        int i = -1;
+        if(nums.length == 0) return 0;
+        int j = 0;
+        while(j<nums.length){
+            if(nums[j]!=val){
+                nums[++i] = nums[j];
+            }
+            j++;
+        }
+        return i+1;
+
+    }
+}
