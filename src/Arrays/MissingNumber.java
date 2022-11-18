eeb6cdac882957728f2990377a7b1c7aef2be7d4
klaus19 <tejaskhartude@gmail.com>
2022-11-18 19:33:18 +0530

    Added Missing number problem

---
+++ src/Arrays/MissingNumber.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class MissingNumber {
+
+    public int missingNumber(int[] nums) {
+
+            int sum = 0;
+            for(int num: nums)
+                sum += num;
+
+            return (nums.length * (nums.length + 1) )/ 2 - sum;
+        }
+
+    }
+
